6450a94cf0034ad2914f2c2ebb9b31ccec6e763d
Konstantin Perikov <pericov.ko@gmail.com>
2022-05-04 19:55:29 +0100

    add simple test

--- test_corner_finder.py
+++ test_corner_finder.py
@@ -1,6 +1,9 @@
+import numpy as np
+
 from corner_finder import closest_node
 
 
 def test_closest_node():
     # testing that the function returns the correct node
-    assert closest_node([1, 1], [[0, 0], [1, 1], [2, 2]]) == [1, 1]
+    node = closest_node((0, 0), [(0, 0), (1, 1), (2, 2)])
+    np.testing.assert_array_equal(node, np.asarray((0, 0)))
